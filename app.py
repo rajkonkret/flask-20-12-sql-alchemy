3c51477a2d7d91e9109829b1be3a3d005051a2e7
rajkonkret <rajkonkret660@gmail.com>
2023-12-22 15:09:17 +0100

    3 - add SQLALCHEMY

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
